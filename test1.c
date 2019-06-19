a6c5652b78eb8d2c923ddd8d934f830cac65bad8
root <illegalism666@gmail.com>
2019-06-19 04:01:02 +0300

    ikinci

--- test1.c
+++ test1.c
@@ -5,14 +5,14 @@ printf("Bir reqem yazin> "
 "\n1-WEB attack"
 "\n2-Wirelless Attack"
 "\n3-Bluetooth Attack\n"
-"\n0-Exit\n");
+"0-Exit\n");
 scanf("%d",&select);
 if(select==1)
 printf("%d >Web attack\n",select);
 else if(select==2)
 printf("%d >Wirelless Attack\n",select);
 else if(select==3)
-printf("%d > Bluetooth Attack",select);
+printf("%d > Bluetooth Attack\n",select);
 else if(select == 0)
 printf("%d > Cixis\n",select);
 else
