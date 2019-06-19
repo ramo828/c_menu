7f7436270f5352ef2fd58da0a64e0b497c008012
root <root@localhost.localdomain>
2019-06-19 03:47:06 +0300

    ilk

--- test1.c
+++ test1.c
@@ -4,7 +4,8 @@ int select;
 printf("Bir reqem yazin> "
 "\n1-WEB attack"
 "\n2-Wirelless Attack"
-"\n3-Bluetooth Attack\n");
+"\n3-Bluetooth Attack\n"
+"\n0-Exit\n");
 scanf("%d",&select);
 if(select==1)
 printf("%d >Web attack\n",select);
@@ -12,7 +13,9 @@ else if(select==2)
 printf("%d >Wirelless Attack\n",select);
 else if(select==3)
 printf("%d > Bluetooth Attack\n",select);
+else if(select == 0)
+printf("%d > Cixis\n",select);
 else
-printf("Bilinmeyen bir emr>\n");
+printf("Sehv: Bilinmeyen bir emr>\n");
 return 0;
 }
